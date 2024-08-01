a65efa278623add7911734409b4ab38e055b7192
Adri-Ferreira <adriano.xevolution@gmail.com>
2024-08-01 11:56:47 +0100

    conclusão do código

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+//Definir a class Heroi
 class Heroi{
     constructor(nome, idade, tipo, ataque){
+        //adicionar as propriedades
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
         this.ataque = ataque;
     }
+//Retorna uma string que descreve a ação do heroi
     toString(){
         console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
     }
 }
-let nome = new Heroi("Arganile", 19, "mago", "magia" )    
+//Criar as instâncias da class Heroi
+let mago = new Heroi("Arganile", 19, "mago", "magia" )    
+let guerreiro = new Heroi("Aragon", 24, "guerreiro", "espada");
+let monge = new Heroi("Ragnar", 32, "monge", "artes marciais");
+let ninja = new Heroi("Taki", 25, "ninja", "shuriken");
 
-nome.toString()
+//Chamar o método toString e exibir no console
+mago.toString()
+guerreiro.toString();
+monge.toString();
+ninja.toString();
